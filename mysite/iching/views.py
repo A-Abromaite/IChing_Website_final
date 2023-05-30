728492999e2c285e72009cd8019b4994f7063eaf
Agne Abromaite <agne.abromaite@gmail.com>
2023-05-30 23:35:23 +0300

    views changed so that toss_coins.html button can only be clicked 6 times

--- mysite/iching/views.py
+++ mysite/iching/views.py
@@ -34,14 +34,28 @@ def cast_results(results):
 
 def toss_coins(request):
     if request.method == "POST":
-        results = generate_results()
-        casted_results = cast_results(results)
+        button_clicks = request.session.get('button_clicks', 0)
+        if button_clicks < 6:
+            results = generate_results()
+            casted_results = cast_results(results)
+            session_results = request.session.get('casted_results', [])
+            session_results.insert(0, casted_results)
+            request.session['casted_results'] = session_results[:6]  # Limit to 6 rows
+            button_clicks += 1
+            request.session['button_clicks'] = button_clicks
+        else:
+            # Handle the case when button clicks exceed 6 todo
+            results = []
+            casted_results = []
     else:
         results = []
         casted_results = []
+        request.session['casted_results'] = []
+        request.session['button_clicks'] = 0
 
     context = {
         'toss_results': results,
-        'casted_results': casted_results
+        'casted_results': request.session['casted_results']
     }
     return render(request, "toss_coins.html", context=context)
+
