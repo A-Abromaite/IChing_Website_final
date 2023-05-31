25aa5864b2fba04a04edb4081404dec0ba918ec3
Agne Abromaite <agne.abromaite@gmail.com>
2023-05-31 21:20:01 +0300

    models' strs added

--- mysite/iching/models.py
+++ mysite/iching/models.py
@@ -8,12 +8,17 @@ class Coin(models.Model):
     ]
     side = models.CharField(max_length=5, choices=COIN_SIDES, default="Heads")
 
+    def __str__(self):
+        return f"{self.side}"
 class CoinTossCombination(models.Model):
     coin1 = models.ForeignKey(to="Coin", verbose_name="coin1", related_name="combination_coin1", on_delete=models.SET_NULL, null=True)
     coin2 = models.ForeignKey(to="Coin", verbose_name="coin2", related_name="combination_coin2", on_delete=models.SET_NULL, null=True)
     coin3 = models.ForeignKey(to="Coin", verbose_name="coin3", related_name="combination_coin3",on_delete=models.SET_NULL, null=True)
     name = models.CharField(verbose_name="combination", max_length=3, default="")
     casted_result = models.ForeignKey(to="CastedResult", verbose_name="casted_result", on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"{self.name}"
 
 class CastedResult(models.Model):
     CASTED_RESULT = [
@@ -25,9 +30,14 @@ class CastedResult(models.Model):
 
     name = models.CharField(verbose_name="casted_result", max_length=3, choices=CASTED_RESULT, null=True)
 
+    def __str__(self):
+        return f"{self.name}"
+
 class Hexagram(models.Model):
     number = models.CharField(verbose_name="Number", max_length=80, help_text="Enter the number of the hexagram, eg. Hexagram 3")
     description = models.CharField(verbose_name="Description", max_length=100, help_text="Enter description of the hexagram")
     meaning = models.CharField(verbose_name="meaning", max_length=200)
 
+    def __str__(self):
+        return f"{self.number}"
 
