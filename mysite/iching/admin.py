5d09e02e2590cda7595fa3edcb3f211a37102f18
Agne Abromaite <agne.abromaite@gmail.com>
2023-05-31 21:08:16 +0300

    models added, admin.py adjusted accordingly

--- mysite/iching/admin.py
+++ mysite/iching/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
-from .models import Hexagram
+from .models import Hexagram, Coin, CoinTossCombination, CastedResult
+
+class CoinAdmin(admin.ModelAdmin):
+    list_display = ["side"]
+
+class CoinTossCombinationAdmin(admin.ModelAdmin):
+    list_display = ["name", "casted_result"]
+
+class CastedResultAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+class HexagramAdmin(admin.ModelAdmin):
+    list_display = ["number"]
 
 # Register your models here.
-
+admin.site.register(Coin, CoinAdmin)
+admin.site.register(CoinTossCombination, CoinTossCombinationAdmin)
+admin.site.register(CastedResult, CastedResultAdmin)
 admin.site.register(Hexagram)
--- mysite/iching/models.py
+++ mysite/iching/models.py
@@ -1,15 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Coin(models.Model):
+    COIN_SIDES = [
+        ("Heads", "Heads"),
+        ("Tails", "Tails"),
+    ]
+    side = models.CharField(max_length=5, choices=COIN_SIDES, default="Heads")
+
+class CoinTossCombination(models.Model):
+    coin1 = models.ForeignKey(to="Coin", verbose_name="coin1", related_name="combination_coin1", on_delete=models.SET_NULL, null=True)
+    coin2 = models.ForeignKey(to="Coin", verbose_name="coin2", related_name="combination_coin2", on_delete=models.SET_NULL, null=True)
+    coin3 = models.ForeignKey(to="Coin", verbose_name="coin3", related_name="combination_coin3",on_delete=models.SET_NULL, null=True)
+    name = models.CharField(verbose_name="combination", max_length=3, default="")
+    casted_result = models.ForeignKey(to="CastedResult", verbose_name="casted_result", on_delete=models.SET_NULL, null=True)
+
+class CastedResult(models.Model):
+    CASTED_RESULT = [
+        ("HHT", "HHT"),
+        ("TTH", "TTH"),
+        ("HHH", "HHH"),
+        ("TTT", "TTT"),
+    ]
+
+    name = models.CharField(verbose_name="casted_result", max_length=3, choices=CASTED_RESULT, null=True)
+
 class Hexagram(models.Model):
     number = models.CharField(verbose_name="Number", max_length=80, help_text="Enter the number of the hexagram, eg. Hexagram 3")
     description = models.CharField(verbose_name="Description", max_length=100, help_text="Enter description of the hexagram")
     meaning = models.CharField(verbose_name="meaning", max_length=200)
 
-class CoinTossResult(models.Model):
-    COIN_SIDES = (
-        ('Heads', 'Heads'),
-        ('Tails', 'Tails'),
-    )
-    toss = models.CharField(verbose_name="toss", max_length=5, choices=COIN_SIDES)
-    timestamp = models.DateTimeField(auto_now_add=True)
+
