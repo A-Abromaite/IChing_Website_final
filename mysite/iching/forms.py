e40980d67b02b5afe6747e0f67204fa645153286
Agne Abromaite <agne.abromaite@gmail.com>
2023-06-11 16:31:14 +0300

    footer moved to index.html only

--- mysite/iching/forms.py
+++
@@ -1,8 +0,0 @@
-from .models import HexagramInstance
-from django import forms
-from django.contrib.auth.models import User
-
-class HexagramInstanceForm(forms.ModelForm):
-    class Meta:
-        model = HexagramInstance
-        fields = ["hexagram_number", "modified_hexagram_number", "note"]
